6eab92b32cdfe78fedeba5682007dd6500c99978
anotoniojmendes <donutsrool@gmail.com>
2019-03-25 12:59:27 +0000

    CSS

--- complete.php
+++ complete.php
@@ -4,10 +4,12 @@
 	session_start();
 	
 	//captar os dados recebidos do formulário com o método POST
-    $id_task=$_POST['id_task'];
-    $completed_at=$_POST['completed_at'];
+    $id_task=$_POST['id'];
 	
-	$altera="UPDATE tasks SET `completed_at`='$completed_at' WHERE `id_task`='$id_task'";
+	date_default_timezone_set('UTC');
+	$date = date("Y-m-d H:i:s");
+	
+	$altera="UPDATE tasks SET `completed_at`='$date' WHERE `id_task`='$id_task'";
 	
 	$resultado =DBExecute($altera);
 	
